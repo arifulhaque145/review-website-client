4919ec53cbebbb6fa87a3112dd84a71d82bd4b33
Ariful145 <arifulh145@gmail.com>
2021-10-04 20:35:52 +0600

    bug fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
       <div className="bg-black p-4">
         <h2 className="text-white text-center">
-          lexicon webiste @ copywrite 2021
+          lexicon website @ copywrite 2021
         </h2>
         <div className="flex justify-center py-4">
           <i className="text-white px-2 text-xl fab fa-facebook-square"></i>
